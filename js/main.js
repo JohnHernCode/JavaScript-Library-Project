175a7b4fe0b66325b12b85e3c7b70b7f15a896a4
jadx2 <delmarjaim@gmail.com>
2021-04-28 12:27:08 -0500

    create table to show books info

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
 const tableBody = document.querySelector('.table-body');
-console.log('hello');
 
 let myLibrary = [
   {
+    id: 1,
     title: 'Fight Club',
     author: 'Chuck Palahniuk',
     pages: 245,
-    read: 'read',
+    bookStatus: 'read',
   },
   {
+    id: 2,
     title: 'Hat Full of Sky',
     author: 'Terry Pratchet',
     pages: 354,
-    read: 'read',
+    bookStatus: 'read',
   },
 ];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, bookStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.bookStatus = bookStatus;
   this.info = function () {
-    return `${title} by ${author}, ${pages}, ${read}`;
+    return `${title} by ${author}, ${pages}, ${bookStatus}`;
   };
 }
 
@@ -33,9 +34,21 @@ function addBookToLibrary() {
 function showBooks(arr) {
   for (let i = 0; i < arr.length; i++) {
     let row = document.createElement('tr');
-    let col = document.createElement('td');
-    col.innerHTML = `${arr[i].title}`;
-    row.appendChild(col);
+    let id = document.createElement('td');
+    let title = document.createElement('td');
+    let author = document.createElement('td');
+    let pages = document.createElement('td');
+    let bookStatus = document.createElement('td');
+    id.innerHTML = `${arr[i].id}`;
+    title.innerHTML = `${arr[i].title}`;
+    author.innerHTML = `${arr[i].author}`;
+    pages.innerHTML = `${arr[i].pages}`;
+    bookStatus.innerHTML = `${arr[i].bookStatus}`;
+    row.appendChild(id);
+    row.appendChild(title);
+    row.appendChild(author);
+    row.appendChild(pages);
+    row.appendChild(bookStatus);
     tableBody.appendChild(row);
   }
 }
