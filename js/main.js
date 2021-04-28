703d1f1e2a353d42835ae7687c8a4020496f88c1
jadx2 <delmarjaim@gmail.com>
2021-04-28 12:01:40 -0500

    add showBook logic

--- js/main.js
+++ js/main.js
@@ -1,38 +1,50 @@
-let myLibrary = [{title: "Fight Club", author: "Chuck Palahniuk", pages: 245, read: "read" }, {title: "Hat Full of Sky", author: "Terry Pratchet", pages: 354, read: "read" }]
+// const tableBody = document.querySelector('.table-body');
+console.log('hello');
 
-function book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.info = function(){
-    return `${title} by ${author}, ${pages}, ${read}`
-  }
-}
+// let myLibrary = [
+//   {
+//     title: 'Fight Club',
+//     author: 'Chuck Palahniuk',
+//     pages: 245,
+//     read: 'read',
+//   },
+//   {
+//     title: 'Hat Full of Sky',
+//     author: 'Terry Pratchet',
+//     pages: 354,
+//     read: 'read',
+//   },
+// ];
 
-function addBookToLibrary() {
-  // do stuff here
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+//   this.info = function () {
+//     return `${title} by ${author}, ${pages}, ${read}`;
+//   };
+// }
 
+// function addBookToLibrary() {
+//   // do stuff here
+// }
 
-}
-
-function showBooks() {
-  // <tr>
-  //         <th scope="row">1</th>
-  //         <td>Mark</td>
-  //         <td>Otto</td>
-  //         <td>@mdo</td>
-  //       </tr>
-  //       <tr>
-  //         <th scope="row">2</th>
-  //         <td>Jacob</td>
-  //         <td>Thornton</td>
-  //         <td>@fat</td>
-  //       </tr>
-  //       <tr>
-  //         <th scope="row">3</th>
-  //         <td colspan="2">Larry the Bird</td>
-  //         <td>@twitter</td>
-  //       </tr>
-}
-
+// function showBooks(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let row = document.createElement('tr');
+//     let col = document.createElement('td');
+//     row.element.classList.add('row');
+//     col.element.classList.add('col');
+//     col.innerHTML = `${arr[i].title}`;
+//     row.appendChild(row);
+//     tableBody.appendChild(col);
+//   }
+//   showBooks(myLibrary);
+//   // <tr>
+//   //         <th scope="row">1</th>
+//   //         <td>Mark</td>
+//   //         <td>Otto</td>
+//   //         <td>@mdo</td>
+//   //       </tr>
+// }
