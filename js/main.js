64c0b7cd0049285bb8fa8190b7c9d6492b0ee18f
JohnHernCode <besthostandseoinfo@gmail.com>
2021-04-28 11:08:29 -0600

    Add Show Books Function

--- js/main.js
+++ js/main.js
@@ -1,50 +1,43 @@
-// const tableBody = document.querySelector('.table-body');
+const tableBody = document.querySelector('.table-body');
 console.log('hello');
 
-// let myLibrary = [
-//   {
-//     title: 'Fight Club',
-//     author: 'Chuck Palahniuk',
-//     pages: 245,
-//     read: 'read',
-//   },
-//   {
-//     title: 'Hat Full of Sky',
-//     author: 'Terry Pratchet',
-//     pages: 354,
-//     read: 'read',
-//   },
-// ];
+let myLibrary = [
+  {
+    title: 'Fight Club',
+    author: 'Chuck Palahniuk',
+    pages: 245,
+    read: 'read',
+  },
+  {
+    title: 'Hat Full of Sky',
+    author: 'Terry Pratchet',
+    pages: 354,
+    read: 'read',
+  },
+];
 
-// function Book(title, author, pages, read) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-//   this.info = function () {
-//     return `${title} by ${author}, ${pages}, ${read}`;
-//   };
-// }
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages}, ${read}`;
+  };
+}
 
-// function addBookToLibrary() {
-//   // do stuff here
-// }
+function addBookToLibrary() {
+  // do stuff here
+}
 
-// function showBooks(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     let row = document.createElement('tr');
-//     let col = document.createElement('td');
-//     row.element.classList.add('row');
-//     col.element.classList.add('col');
-//     col.innerHTML = `${arr[i].title}`;
-//     row.appendChild(row);
-//     tableBody.appendChild(col);
-//   }
-//   showBooks(myLibrary);
-//   // <tr>
-//   //         <th scope="row">1</th>
-//   //         <td>Mark</td>
-//   //         <td>Otto</td>
-//   //         <td>@mdo</td>
-//   //       </tr>
-// }
+function showBooks(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let row = document.createElement('tr');
+    let col = document.createElement('td');
+    col.innerHTML = `${arr[i].title}`;
+    row.appendChild(col);
+    tableBody.appendChild(row);
+  }
+}
+
+showBooks(myLibrary);
